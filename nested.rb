b38f1e21d5d39ddabd92eb1d59e1a663de6e1073
Kelsea McAllister <kelsea.mcall@gmail.com>
2020-10-09 17:20:42 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -81,7 +81,7 @@ def adding_matz
         :languages => ["C"]
       }
     }
-
+	programmer_hash[:yukihiro_matsumoto] = {:known_for => "Ruby", :languages => ["LIPS", "C"]}
 end
 
 def changing_alan
